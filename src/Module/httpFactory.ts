a83a7b71a00ee307662d5bb44b9b76319a25d7d9
d.sidali <d.sidali@xpertsoft-dz.com>
2018-11-18 08:03:55 +0000

    Nettoyer le trunc pour mettre la version Xamarin

--- src/Module/httpFactory.ts
+++
@@ -1,7 +0,0 @@
-import { AuthServiceProvider } from './../providers/auth-service/auth-service';
-import {XHRBackend, Http, RequestOptions} from "@angular/http";
-import {HttpInterceptor} from "./httpInterceptor";
-
-export function HttpFactory(xhrBackend: XHRBackend, requestOptions: RequestOptions,authService: AuthServiceProvider): Http {
-    return new HttpInterceptor(xhrBackend, requestOptions,authService);
-}
