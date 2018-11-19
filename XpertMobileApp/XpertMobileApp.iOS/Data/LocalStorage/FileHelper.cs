43dda1260beba12b1e970dc616cb50df6043c8d0
d.sidali <d.sidali@xpertsoft-dz.com>
2018-11-19 15:08:51 +0000

    SQLite storage pour les trois plateforme Android, IOS et Windows app

---
+++ XpertMobileApp/XpertMobileApp.iOS/Data/LocalStorage/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using XpertMobileApp.iOS.Data;
+using XpertMobileApp.Data;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace XpertMobileApp.iOS.Data
+{
+	public class FileHelper : IFileHelper
+	{
+		public string GetLocalFilePath(string filename)
+		{
+			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+			if (!Directory.Exists(libFolder))
+			{
+				Directory.CreateDirectory(libFolder);
+			}
+
+			return Path.Combine(libFolder, filename);
+		}
+	}
+}
