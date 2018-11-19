b788ee2687fe3eb5cd5d017ebbb9cd6f02bb9eba
d.sidali <d.sidali@xpertsoft-dz.com>
2018-11-19 14:59:31 +0000

    Class de base de l'application

---
+++ XpertMobileApp/XpertMobileApp/Models/Settings.cs
@@ -0,0 +1,61 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XpertMobileApp.Models
+{
+    public class Settings : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool isModified;
+
+        [PrimaryKey]
+        public int Id { get; set; }
+
+        private string serverName { get; set; }
+
+        private string port { get; set; }
+
+        private bool autoRefreshVente { get; set; }
+
+        public string Port
+        {
+            set
+            {
+                if (port != value)
+                {
+                    port = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Port"));
+                    isModified = true;
+                }
+            }
+            get
+            {
+                return port;
+            }
+        }
+
+        public string ServerName
+        {
+            set
+            {
+                if (serverName != value)
+                {
+                    serverName = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ServerName"));
+                    isModified = true;
+                }
+            }
+            get
+            {
+                return serverName;
+            }
+        }
+
+        public Settings() { }
+
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XpertMobileApp.Models
+{
+    public class User
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        public User() { }
+
+        public User(string userName, string passWord)
+        {
+            UserName = userName;
+            PassWord = passWord;
+        }
+    }
+}
