58661f1e69367b677a60637e74151c6ec794327c
d.sidali <d.sidali@xpertsoft-dz.com>
2018-11-20 15:22:30 +0000

    Class de vérification que le device est connecté a internet.

---
+++ XpertMobileApp/XpertMobileApp.Android/Data/NetworkConnection.cs
@@ -0,0 +1,22 @@
+﻿using Android.Content;
+using Android.Net;
+using itMobileApp.Data;
+using Xamarin.Forms;
+using XpertMobileApp.Droid.Data;
+
+[assembly: Dependency(typeof(NetworkConnection))]
+
+namespace XpertMobileApp.Droid.Data
+{
+    public class NetworkConnection : INetworkConnection
+    {
+        public bool IsConnected { get; set; }
+
+        public void CheckNetworkConnexction()
+        {
+            var connectivityManager = (ConnectivityManager)Android.App.Application.Context.GetSystemService(Context.ConnectivityService);
+            var activeNetworkInfo = connectivityManager.ActiveNetworkInfo;
+            IsConnected = (activeNetworkInfo != null && activeNetworkInfo.IsConnectedOrConnecting);
+        }
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Data/INetworkConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace itMobileApp.Data
+{
+    public interface INetworkConnection
+    {
+        bool IsConnected { get; }
+        void CheckNetworkConnexction();
+    }
+}
