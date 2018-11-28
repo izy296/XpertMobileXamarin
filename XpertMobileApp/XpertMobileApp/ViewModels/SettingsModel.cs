362bfdda43c69943bd5533060c49d245aab37bb9
d.sidali <d.sidali@xpertsoft-dz.com>
2018-11-28 15:41:26 +0000

    Page de configuration de l'application

---
+++ XpertMobileApp/XpertMobileApp/ViewModels/SettingsModel.cs
@@ -0,0 +1,70 @@
+﻿using XpertMobileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using XpertMobileApp.Model;
+using System.Linq;
+
+namespace XpertMobileApp.ViewModels
+{
+    public class SettingsModel : BaseViewModel
+    {
+
+        public Settings Settings { get => App.Settings; set => App.Settings = value; }
+
+        public ObservableCollection<Language> Languages { get; }
+
+        public bool IsAdminUser
+        {
+            get
+            {
+                return App.CurrentUser != null;
+            }
+        }
+
+        public SettingsModel()
+        {
+            Title = "Configuration";
+
+            Languages = new ObservableCollection<Language>()
+            {
+                new Language { DisplayName =  "Device language", ShortName = "" },
+                new Language { DisplayName =  "عربي", ShortName = "ar" },
+                new Language { DisplayName =  "English", ShortName = "en" },
+                new Language { DisplayName =  "Français - French", ShortName = "fr" },
+                new Language { DisplayName =  "中文 - Chinese (simplified)", ShortName = "zh-Hans" }
+            };
+
+            LoadSettings();
+        }
+
+        public Language GetLanguageElem(string language)
+        {
+            Language result = Languages[0];
+            if (Languages.Where( e => e.ShortName == language).Count() > 0)
+            {
+                result = Languages.Where(e => e.ShortName == language).ToList()[0];
+            }
+            return result;
+        }
+
+
+        public void LoadSettings()
+        {
+            this.Settings = App.SettingsDatabase.GetFirstItemAsync().Result;
+            if (this.Settings == null)
+            {
+                this.Settings = new Settings();
+            }
+            this.Settings.isModified = false;           
+        }
+
+        public void SaveSettings()
+        {
+            App.SettingsDatabase.SaveItemAsync(Settings);
+            this.Settings.isModified = false;
+        }
+    }
+}
+
---
+++ XpertMobileApp/XpertMobileApp/Views/SettingsPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using XpertMobileApp.ViewModels;
+using Plugin.Connectivity;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using XpertMobileApp.Model;
+
+namespace XpertMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsPage : ContentPage
+	{
+
+        public ObservableCollection<Language> Languages { get; }
+
+        public SettingsModel viewModel;
+
+        public SettingsPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new SettingsModel();
+
+            LanguagesPicker.SelectedItem = viewModel.GetLanguageElem(viewModel.Settings.Language);
+        }
+
+
+        public SettingsPage (bool isModal = false)
+		{
+			InitializeComponent();
+            CommandeGrid.IsVisible = isModal;
+            BindingContext = viewModel = new SettingsModel();
+
+            LanguagesPicker.SelectedItem = viewModel.GetLanguageElem(viewModel.Settings.Language);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            LanguagesPicker.SelectedIndexChanged += LanguagesPicker_SelectedIndexChanged;
+        }
+
+        private void LanguagesPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var language = viewModel.Languages[LanguagesPicker.SelectedIndex];
+
+            App.SetAppLanguage(language.ShortName);
+
+            lbl_LanguageSelector.Text = AppResources.sp_lbl_SelectLanguage;
+            lbl_ServerName.Text       = AppResources.sp_lbl_ServerName;
+            lbl_PortNumber.Text       = AppResources.sp_lbl_PortNumber;
+            Btn_CloseSettings.Text    = AppResources.btn_Close;
+            Btn_SaveSettings.Text     = AppResources.sp_btn_SaveSettings;
+            Btn_TestCnx.Text          = AppResources.sp_btn_TestConnection;
+            lbl_ConnexionInfos.Text   = AppResources.sp_lbl_ConnexionInfos;
+        }
+
+        protected override async void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            if (viewModel.Settings.isModified)
+            { 
+                var action = await DisplayAlert(AppResources.alrt_msg_title_Settings, AppResources.alrt_msg_SaveSettings,
+                    AppResources.alrt_msg_Ok, AppResources.alrt_msg_Cancel);
+                if (action)
+                {
+                    viewModel.SaveSettings();
+                }
+            }
+        }
+
+        protected void SettingsClose_Clicked(object sender, EventArgs e)
+        {
+            this.Navigation.PopModalAsync(true);
+        }
+
+        protected void SaveSettings_Clicked(object sender, EventArgs e)
+        {
+            viewModel.SaveSettings();
+            DisplayAlert(AppResources.alrt_msg_Info, AppResources.alrt_msg_SettingsSaved, AppResources.alrt_msg_Ok);
+        }
+
+        protected async void TestConnexion_Clicked(object sender, EventArgs e)
+        {
+            // Check if the WebService is configured
+            if (string.IsNullOrEmpty(App.RestServiceUrl))
+            {
+                await DisplayAlert(AppResources.alrt_msg_Info, AppResources.alrt_msg_MissingServerInfos, AppResources.alrt_msg_Ok);
+                return;
+            }
+
+            bool reachable = await this.IsBlogReachableAndRunning(App.Settings.ServerName, Convert.ToInt32(App.Settings.Port));
+            if (reachable)
+            {
+                await DisplayAlert(AppResources.alrt_msg_Info, AppResources.alrt_msg_ConnectionSucces, AppResources.alrt_msg_Ok);
+            }
+            else
+            {
+                await DisplayAlert(AppResources.alrt_msg_Info, AppResources.alrt_msg_ConnectionError, AppResources.alrt_msg_Ok);
+            }
+        }
+
+        public async Task<bool> IsBlogReachableAndRunning(string host, int port = 80, int msTimeout = 5000)
+        {
+            var connectivity = CrossConnectivity.Current;
+            if (!connectivity.IsConnected)
+                return false;
+
+            var reachable = await connectivity.IsRemoteReachable(host, port, msTimeout);
+
+            return reachable;
+        }
+    }
+}
