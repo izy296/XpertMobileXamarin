007c4bc04b4b88ba71ce4558c9b76e7f8d62e5e5
d.sidali <d.sidali@xpertsoft-dz.com>
2018-11-29 20:26:36 +0000

    Conception de l'affichage de la page accueil

--- XpertMobileApp/XpertMobileApp/Views/HomePage.xaml.cs
+++ XpertMobileApp/XpertMobileApp/Views/HomePage.xaml.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xpert.Pharm.DAL;
+using XpertMobileApp.Helpers;
+using XpertMobileApp.Models;
 
 namespace XpertMobileApp.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HomePage : ContentPage
-	{
-		public HomePage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        async Task ConnectUserAsync(object sender, EventArgs e)
+        {
+            await GetSessionInfos();
+        }
+
+        internal async Task<List<TRS_JOURNEES>> GetSessionInfos()
+        {
+            string url = App.RestServiceUrl + ServiceUrlDeco.SESSION_INFO_URL;
+
+            Token tokenInfos = await App.TokenDatabase.GetFirstItemAsync();
+            
+            List<TRS_JOURNEES> resultData = await WSApi.ExecuteGet<List<TRS_JOURNEES>>(url, App.User.Token.access_token);
+            //string resposeData = Encoding.UTF8.GetString(resultData);
+            //result = JsonConvert.DeserializeObject<VteServiceResponse>(resposeData);
+
+            return resultData;
+        }
+
+    }
 }
