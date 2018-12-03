428891ce98a146fdc0fc9f66260fd92759846b9b
d.sidali <d.sidali@xpertsoft-dz.com>
2018-12-03 16:27:15 +0000

    Page du listing des encaissements

---
+++ XpertMobileApp/XpertMobileApp/ViewModels/Encaissement/EncaissementsViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xpert.Pharm.DAL;
+using XpertMobileApp.Services;
+
+namespace XpertMobileApp.ViewModels
+{
+    public class EncaissementsViewModel : BaseViewModel
+    {
+        public ObservableCollection<View_TRS_ENCAISS> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
+        public EncaissementsViewModel()
+        {
+            Title = AppResources.pn_encaissement;
+
+            Items = new ObservableCollection<View_TRS_ENCAISS>();
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+
+/*
+            MessagingCenter.Subscribe<NewItemPage, Item>(this, "Ajout", async (obj, item) =>
+            {
+                var newItem = item as Item;
+                Items.Add(newItem);
+                await DataStore.AddItemAsync(newItem);
+            });
+*/
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await WebServiceClient.GetEncaissements(App.RestServiceUrl, "all", "1", "all", "", "", "", "", "");
+                int index = 1;
+                foreach (var item in items)
+                {
+                    item.Index = index;
+                    Items.Add(item);
+                    index += 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+
+}
---
+++ XpertMobileApp/XpertMobileApp/Views/Encaissement/EncaissementsPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xpert.Pharm.DAL;
+using XpertMobileApp.ViewModels;
+
+namespace XpertMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EncaissementsPage : ContentPage
+	{
+        EncaissementsViewModel viewModel;
+
+        public EncaissementsPage ()
+		{
+			InitializeComponent ();
+            BindingContext = viewModel = new EncaissementsViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as TRS_ENCAISS;
+            if (item == null)
+                return;
+
+      //      await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+              viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
