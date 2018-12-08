8f58bbd0016ad48847f2d21e5d431aef533ff519
d.sidali <d.sidali@xpertsoft-dz.com>
2018-12-08 19:59:33 +0000

    Afficheur générique d'objet

---
+++ XpertMobileApp/XpertMobileApp/ViewModels/Items/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xpert.Pharm.DAL;
+using XpertMobileApp.Models;
+
+namespace XpertMobileApp.ViewModels
+{
+    public class ItemDetailViewModel<T> : BaseViewModel
+    {
+        public T Item { get; set; }
+        public ItemDetailViewModel(T item)
+        {
+            Title = item?.ToString();
+            Item = item;
+        }
+    }
+}
