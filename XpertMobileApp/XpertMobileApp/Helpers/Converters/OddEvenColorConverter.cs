c8ea54d082aeafb4d1f00c222318e012a2d1a90b
d.sidali <d.sidali@xpertsoft-dz.com>
2018-12-09 20:29:08 +0000

    Création d'un Converter pour afficher les Odd Even dans une liste view

---
+++ XpertMobileApp/XpertMobileApp/Helpers/Converters/OddEvenColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XpertMobileApp.Converters
+{
+    public class OddEvenColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int index = (int)value;
+            if ((index % 2) == 0)
+                return Color.Green;
+              else
+                return Color.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Only one way bindings are supported with this converter");
+        }
+    }
+}
