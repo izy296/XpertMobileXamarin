5e70ea849ccd80bd6d20b3b7351785699c78776e
d.sidali <d.sidali@xpertsoft-dz.com>
2019-01-16 16:32:51 +0000

    XPHARM5-62806 : Fonctionnalités diverses & utilités
    * ressources non comité.

---
+++ XpertMobileApp/XpertMobileApp.Android/Services/CloseApplication.cs
@@ -0,0 +1,16 @@
+﻿using Java.Lang;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(XpertMobileApp.Droid.Services.CloseApplication))]
+namespace XpertMobileApp.Droid.Services
+{
+    public class CloseApplication : XpertMobileApp.Services.ICloseApplication
+    {
+        public void closeApplication()
+        {
+             JavaSystem.Exit(0);
+            // var activity = (Activity)Forms.Context;
+            // activity.FinishAffinity();
+        }
+    }
+}
