42ce8385c9fa3db873fecfd5bc884d4441137801
r.soufiane <s.rahim@xpertsoft-dz.com>
2019-03-27 13:33:03 +0000

    Interface RFScaner

---
+++ XpertMobileApp/XpertMobileApp/Api/Services/RFID/IRfidScaner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace XpertMobileApp.Services
+{
+    public interface IRfidScaner
+    {
+        string ConvertUiiToEPC(string val);
+
+        string[] ReadTagFromBuffer();
+
+         void StopInventory();
+
+        string InventorySingleTag();
+        bool SatrtContenuesInventary(byte anti, byte q);
+        void GetInstance();
+        void Init();
+    }
+}
