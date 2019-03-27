171f19b02ad2cf2bc36011f91f9385f4b48af174
d.sidali <d.sidali@xpertsoft-dz.com>
2019-03-27 14:53:22 +0000

    Rfid scaner interface

---
+++ XpertMobileApp/XpertMobileApp/Views/Rfid/RfidScanPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Linq;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Helpers;
+using XpertMobileApp.ViewModels;
+
+namespace XpertMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RfidScanPage : ContentPage
+	{
+        RfidScanViewModel viewModel;
+
+        public RfidScanPage()
+		{
+			InitializeComponent ();
+
+            BindingContext = viewModel = new RfidScanViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            viewModel.Items.Add("string1");
+            viewModel.Items.Add("string2");
+            viewModel.Items.Add("string3");
+        }
+
+        private void Filter_Clicked(object sender, EventArgs e)
+        {
+            FilterPanel.IsVisible = !FilterPanel.IsVisible;
+        }
+
+        private void btn_Clear_Clicked(object sender, EventArgs e)
+        {
+            viewModel.Items.Clear();
+        }
+
+        private void btn_Scan_Clicked(object sender, EventArgs e)
+        {
+            
+            if (viewModel.ContinuesScan)
+            { 
+                viewModel.StartInventorySingl(viewModel.ContinuesScan);
+            }
+            else
+            {
+                byte anti = 1;
+                byte q = 1;
+                viewModel.SatrtContenuesInventary(anti, q);
+            }
+        }
+
+        private void btn_StopScan_Clicked(object sender, EventArgs e)
+        {
+            viewModel.StopInventory();
+        }
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Views/Rfid/RfidScanViewModel.cs
@@ -0,0 +1,90 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Extended;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Services;
+
+namespace XpertMobileApp.ViewModels
+{
+   public class RfidScanViewModel : BaseViewModel
+   {
+        public IRfidScaner RFScaner = DependencyService.Get<IRfidScaner>();
+
+        public InfiniteScrollCollection<string> Items { get; set; }
+
+        bool loopFlag;
+
+        public int elementsCount { get; set; }
+
+        public bool ContinuesScan { get; set; }
+
+        public int Anti { get; set; }
+
+        public RfidScanViewModel()
+        {
+           Title = AppResources.pn_RfidScan;
+
+            Items = new InfiniteScrollCollection<string>();
+        }
+
+        #region Rfid scaner tool
+
+        public void StopInventory()
+        {
+            RFScaner.StopInventory();
+            loopFlag = false;
+        }
+
+        public void Init()
+        {
+            RFScaner.GetInstance();
+            RFScaner.Init();
+        }
+
+        public void SatrtContenuesInventary(byte anti, byte q)
+        {
+            RFScaner.SatrtContenuesInventary(anti, q);
+            loopFlag = true;
+            ContinuousRead();
+        }
+
+        public string StartInventorySingl(bool Continuous)
+        {
+            return RFScaner.InventorySingleTag();
+        }
+
+        public void ContinuousRead()
+        {
+            Thread th = new Thread(new ThreadStart(delegate
+            {
+                while (loopFlag)
+                {
+                    string[] res = RFScaner.ReadTagFromBuffer();
+                    if (res != null)
+                    {
+                        string strEPC;
+                        string strTid = "";
+                        StringBuilder sb = new StringBuilder();
+                        if (res[0].Length != 0 && res[0] != "0000000000000000" && res[0] != "000000000000000000000000")
+                        {
+                            strTid = "TID:" + res[0] + "\r\n";
+                        }
+                        strEPC = "EPC:" + RFScaner.ConvertUiiToEPC(res[1]) + "@";
+                        sb.Append(strTid);
+                        sb.Append(strEPC);
+                        sb.Append(res[2]);
+
+                        Items.Insert(0, sb.ToString());
+                    }
+                }
+            }));
+            th.Start();
+        }
+        #endregion
+    }
+}
