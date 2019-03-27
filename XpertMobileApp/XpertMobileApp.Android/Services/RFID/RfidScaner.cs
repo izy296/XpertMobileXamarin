d9b189285040931430881cfdc7e2fcd1da7d0150
r.soufiane <s.rahim@xpertsoft-dz.com>
2019-03-27 13:48:01 +0000

    Implimentation android du RFScaner

---
+++ XpertMobileApp/XpertMobileApp.Android/Services/RFID/RfidScaner.cs
@@ -0,0 +1,47 @@
+﻿using Com.Rscja.Deviceapi;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using XpertMobileApp.Services;
+
+[assembly: Dependency(typeof(XpertMobileApp.Droid.Services.RfidScaner))]
+namespace XpertMobileApp.Droid.Services
+{
+    public class RfidScaner : XpertMobileApp.Services.IRfidScaner
+    {
+        private RFIDWithUHF uhfAPI;
+        public string[] ReadTagFromBuffer()
+        {
+            return uhfAPI.ReadTagFromBuffer();
+        }
+        public void StopInventory()
+        {
+            uhfAPI.StopInventory();
+                    
+        }
+        public string InventorySingleTag() {
+            return uhfAPI.InventorySingleTag();
+        }
+           
+        public string ConvertUiiToEPC(string val)
+        {
+            return uhfAPI.ConvertUiiToEPC(val);
+        }
+        public void GetInstance()
+        {
+            uhfAPI = RFIDWithUHF.Instance;
+        }
+        public void Init()
+        {
+            uhfAPI.Init();
+        }
+
+        public bool SatrtContenuesInventary(byte anti, byte q)
+        {
+            return uhfAPI.StartInventoryTag(anti, q);
+           
+           
+        }
+
+
+    }
+}
