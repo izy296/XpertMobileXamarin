42f9c12633acc3a2c5d385ea6d069aa38238527d
d.sidali <d.sidali@xpertsoft-dz.com>
2019-06-18 10:02:45 +0000

    Creation des Managers des opérations crud basé sur le service générique CrudService<T>

---
+++ XpertMobileApp/XpertMobileApp/Api/Managers/CrudManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XpertMobileApp.Api.Managers
+{
+    public static class CrudManager
+    {
+        internal static ProductManager Products = new ProductManager();
+
+
+
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Api/Managers/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xpert.Common.WSClient.Helpers;
+using Xpert.Common.WSClient.Model;
+using Xpert.Common.WSClient.Services;
+using XpertMobileApp.DAL;
+
+namespace XpertMobileApp.Api
+{
+    internal class ProductManager : CrudService<View_STK_PRODUITS>
+    {
+        public ProductManager() : 
+            base(App.RestServiceUrl, "STK_PRODUITS", App.User.Token)
+        {
+
+        }
+
+        public async Task<List<View_STK_PRODUITS>> SelectByCodeBarre(string codeBarre)
+        {
+            string url = GetActionUrl("GetByCodeBarre");
+            url += WSApi2.AddParam(url, "codeBarre", codeBarre);
+
+            return await WSApi2.RetrievAauthorizedData<View_STK_PRODUITS>(url, this.Token.access_token);
+        }
+    }
+}
