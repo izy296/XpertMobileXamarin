69791d7e5190c80bcf103ae627258ce14eb89dc5
d.sidali <d.sidali@xpertsoft-dz.com>
2019-06-20 09:38:42 +0000

    XPMB-70573 : Catalogue produits

---
+++ XpertMobileApp/XpertMobileApp/Views/Catalogue produit/ProduitDetailPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xpert.Common.WSClient.Helpers;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Models;
+using XpertMobileApp.Services;
+using XpertMobileApp.ViewModels;
+
+namespace XpertMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ProduitCDetailPage : ContentPage
+	{
+        ItemRowsDetailViewModel<View_AssistantCommandes, View_STK_STOCK> viewModel;
+
+        private STK_PRODUITS item;
+        public STK_PRODUITS Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public ProduitCDetailPage(STK_PRODUITS prod)
+        {
+            InitializeComponent();
+
+            this.Item = prod;
+        }
+
+        public ProduitCDetailPage(string codeProd)
+        {
+            InitializeComponent();
+
+            Item = new STK_PRODUITS();
+            Item.CODE_PRODUIT = codeProd;
+        }
+
+        private string getFormatedValue(object value)
+        {
+            if (value == null) return "";
+
+            string result = value.ToString();
+
+            if (value is DateTime)
+            {
+                result = string.Format("{0:dd/MM/yyyy HH:mm}", value);
+            }
+            else if (value is decimal)
+            {
+                result = string.Format("{0:F2} DA", value);
+            }
+            else
+            {
+                result = string.Format("{0}", value);
+            }
+
+            return result;
+        }
+
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                if (this.viewModel == null)
+                { 
+                    var ProdInfos = await WebServiceClient.GetProduitDetails(this.Item.CODE_PRODUIT);
+
+                    BindingContext = this.viewModel = new ItemRowsDetailViewModel<View_AssistantCommandes, View_STK_STOCK>(ProdInfos, Item.CODE_PRODUIT);
+                    this.viewModel.LoadRowsCommand = new Command(async () => await ExecuteLoadRowsCommand());
+                }
+                viewModel.LoadRowsCommand.Execute(null);
+            }
+            catch (Exception ex)
+            {
+                await UserDialogs.Instance.AlertAsync(WSApi2.GetExceptionMessage(ex), AppResources.alrt_msg_Alert,
+                    AppResources.alrt_msg_Ok);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        async Task ExecuteLoadRowsCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                viewModel.ItemRows.Clear();
+                var itemsC = await WebServiceClient.GetLots(this.Item.CODE_PRODUIT);
+
+                UpdateItemIndex(itemsC);
+
+                foreach (var itemC in itemsC)
+                {
+                    viewModel.ItemRows.Add(itemC);
+                }
+            }
+            catch (Exception ex)
+            {
+                await UserDialogs.Instance.AlertAsync(WSApi2.GetExceptionMessage(ex), AppResources.alrt_msg_Alert,
+                    AppResources.alrt_msg_Ok);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private void UpdateItemIndex<T>(List<T> items)
+        {
+            int i = 0;
+            foreach (var item in items)
+            {
+                i += 1;
+                (item as BASE_CLASS).Index = i;
+            }
+        }
+
+        private async void RefBtn_Clicked(object sender, EventArgs e)
+        {            
+            await Navigation.PushAsync(new ProduitRefDetailPage(viewModel.Item.REFERENCE));            
+        }
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Views/Catalogue produit/ProduitsCViewModel.cs
@@ -0,0 +1,140 @@
+﻿using Acr.UserDialogs;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xpert.Common.WSClient.Helpers;
+using XpertMobileApp.Api.ViewModels;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Services;
+
+namespace XpertMobileApp.ViewModels
+{
+
+    public class ProduitsCViewModel : CrudBaseViewModel<STK_PRODUITS, View_STK_PRODUITS>
+    {
+        public ProduitsCViewModel()
+        {
+            Title = AppResources.pn_Produits;
+
+            Types = new ObservableCollection<BSE_TABLE_TYPE>();
+            Familles = new ObservableCollection<BSE_TABLE>();
+
+            LoadExtrasDataCommand = new Command(async () => await ExecuteLoadExtrasDataCommand());
+        }
+
+        protected override Dictionary<string, string> GetFilterParams()
+        {
+            Dictionary<string, string> result = base.GetFilterParams();
+
+            result.Add("searchText", SearchedText);
+
+            if (!string.IsNullOrEmpty(SelectedFamille?.CODE))
+                result.Add("famille", SelectedFamille?.CODE);
+
+            if (!string.IsNullOrEmpty(SelectedType?.CODE_TYPE))
+                result.Add("type", SelectedType?.CODE_TYPE);
+
+            return result;
+        }
+
+        protected override void OnAfterLoadItems(IEnumerable<View_STK_PRODUITS> list)
+        {
+            base.OnAfterLoadItems(list);
+
+            int i = 0;
+            foreach (var item in list)
+            {
+                i += 1;
+                item.IMAGE_URL = App.RestServiceUrl + string.Format("/Images/GetImage?codeProduit={0}", item.CODE_PRODUIT);
+                (item as BASE_CLASS).Index = i;
+            }
+        }
+
+        #region filters data
+
+        public string SearchedText { get; set; } = "";
+
+        public ObservableCollection<BSE_TABLE_TYPE> Types { get; set; }
+        public BSE_TABLE_TYPE SelectedType { get; set; }
+
+        public ObservableCollection<BSE_TABLE> Familles { get; set; }
+        public BSE_TABLE SelectedFamille { get; set; }
+
+        async Task ExecuteLoadExtrasDataCommand()
+        {
+
+            if (IsLoadExtrasBusy)
+                return;
+
+            try
+            {
+                IsLoadExtrasBusy = true;
+                await ExecuteLoadFamillesCommand();
+                await ExecuteLoadTypesCommand();
+            }
+            catch (Exception ex)
+            {
+                await UserDialogs.Instance.AlertAsync(WSApi2.GetExceptionMessage(ex), AppResources.alrt_msg_Alert,
+                    AppResources.alrt_msg_Ok);
+            }
+            finally
+            {
+                IsLoadExtrasBusy = false;
+            }
+        }
+
+        async Task ExecuteLoadTypesCommand()
+        {
+
+            try
+            {
+                Types.Clear();
+                var itemsC = await WebServiceClient.GetProduitTypes();
+
+                BSE_TABLE_TYPE allElem = new BSE_TABLE_TYPE();
+                allElem.CODE_TYPE = "";
+                allElem.DESIGNATION_TYPE = AppResources.txt_All;
+                itemsC.Add(allElem);
+
+                foreach (var itemC in itemsC)
+                {
+                    Types.Add(itemC);
+                }
+            }
+            catch (Exception ex)
+            {
+                await UserDialogs.Instance.AlertAsync(WSApi2.GetExceptionMessage(ex), AppResources.alrt_msg_Alert,
+                    AppResources.alrt_msg_Ok);
+            }
+        }
+
+        async Task ExecuteLoadFamillesCommand()
+        {
+            try
+            {
+                Familles.Clear();
+                var itemsC = await WebServiceClient.GetProduitFamilles();
+
+                BSE_TABLE allElem = new BSE_TABLE();
+                allElem.CODE = "";
+                allElem.DESIGNATION = AppResources.txt_All;
+                Familles.Add(allElem);
+
+                foreach (var itemC in itemsC)
+                {
+                    Familles.Add(itemC);
+                }
+            }
+            catch (Exception ex)
+            {
+                await UserDialogs.Instance.AlertAsync(WSApi2.GetExceptionMessage(ex), AppResources.alrt_msg_Alert,
+                    AppResources.alrt_msg_Ok);
+            }
+        }
+
+        #endregion
+    }
+
+}
