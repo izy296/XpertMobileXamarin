7c7e592e15c9861c2e0addca9ecb251349f69058
d.sidali <d.sidali@xpertsoft-dz.com>
2019-11-07 21:30:02 +0000

    [XpertMobile] Module production usine agriculture
    * interface de Vérification

---
+++ XpertMobileApp/XpertMobileApp/Views/Manafiaa/Production/Verification/VerifTiersViewModelcs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XpertMobileApp.ViewModels;
+using XpertMobileApp.DAL;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace XpertMobileApp.Api.ViewModels
+{
+    public class VerifTiersViewModel : BaseViewModel
+    {
+        private View_TRS_TIERS tiers;
+        public View_TRS_TIERS Tiers
+        {
+            get { return tiers; }
+            set
+            {
+                tiers = value;
+                OnPropertyChanged("Tiers");
+            }
+        }
+    
+        public ObservableCollection<View_PRD_AGRICULTURE_DETAIL> ProductionsDetails { get; set; }
+
+        public Command LoadTiersCommand { get; set; }
+
+        public Command LoadProductionsCommand { get; set; }
+
+        public VerifTiersViewModel()
+        {
+            Title = "Vérification";
+        }
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Views/Manafiaa/Production/Verification/VerificationUserPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp.Api.ViewModels;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Helpers;
+using XpertMobileApp.Models;
+using XpertMobileApp.ViewModels;
+using ZXing.Net.Mobile.Forms;
+
+namespace XpertMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VerificationUserPage : ContentPage
+	{
+        VerifTiersViewModel viewModel;
+
+        public VerificationUserPage(string codeTiers)
+        {
+            InitializeComponent();
+            TiersSelector = new TiersSelector();
+            BindingContext = this.viewModel = new VerifTiersViewModel();
+
+            MessagingCenter.Subscribe<TiersSelector, View_TRS_TIERS>(this, MCDico.ITEM_SELECTED, async (obj, selectedItem) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    viewModel.Tiers = selectedItem;
+                });
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+
+        private Task DisplayDetails(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        private TiersSelector TiersSelector;
+        private async void btn_Select_Clicked(object sender, EventArgs e)
+        {
+            TiersSelector.SearchedType = "CF";
+            await PopupNavigation.Instance.PushAsync(TiersSelector);
+        }
+
+        private void btn_Scan_Clicked(object sender, EventArgs e)
+        {
+            var scaner = new ZXingScannerPage();
+            Navigation.PushAsync(scaner);
+            scaner.OnScanResult += (result) =>
+            {
+                scaner.IsScanning = false;
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+
+                    await DisplayDetails(result.Text);
+
+                });
+            };
+        }
+    }
+}
