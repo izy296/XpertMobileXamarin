b33f34cede984495d9e85febd05f1555630a2575
d.sidali <d.sidali@xpertsoft-dz.com>
2019-11-17 18:58:44 +0000

    [XpertMobile] Module production usine agriculture
    * Formulaire d'ajout d'information de tiers Annex a la réception

---
+++ XpertMobileApp/XpertMobileApp/Views/Selector/Anex tiers/AnnexTiersSelector.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Services;
+using XpertMobileApp.ViewModels;
+using XpertMobileApp.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using XpertMobileApp.Models;
+
+namespace XpertMobileApp.Views
+{
+    public partial class AnnexTiersSelector : PopupPage
+    {
+
+        AnnexTiersSelectorViewModel viewModel;
+
+        private List<VIEW_ACH_INFO_ANEX> currentEmballages;
+        public List<VIEW_ACH_INFO_ANEX> CurrentEmballages
+        {
+            get
+            {
+                return viewModel.CurrentEmballages;
+            }
+            set
+            {
+                viewModel.CurrentEmballages = value;
+            }
+        }
+
+        public AnnexTiersSelector()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new AnnexTiersSelectorViewModel();
+
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if(viewModel.Items.Count == 0)
+            {
+               // viewModel.LoadItemsCommand.Execute(null);
+            }
+        }
+
+        private async void OnClose(object sender, EventArgs e)
+        {
+
+            MessagingCenter.Send(this, MCDico.ITEM_SELECTED, viewModel.Items.ToList());
+            
+            await PopupNavigation.Instance.PopAsync();
+        }
+
+        private void ItemsListView_SelectionChanged(object sender, Syncfusion.ListView.XForms.ItemSelectionChangedEventArgs e)
+        {
+            btnSelect.IsEnabled = true;
+        }
+
+        private void btn_ApplyFilter_Clicked(object sender, EventArgs e)
+        {
+            viewModel.Items.Add(new VIEW_ACH_INFO_ANEX()
+            {
+                NOM_TIERS = TiersName.Text,
+                QUANTITE_APPORT = Convert.ToDecimal(TiersQte.Text.Trim())
+                
+            });
+        }
+
+        private void btn_CancelFilter_Clicked(object sender, EventArgs e)
+        {
+            var elem = (sender as Button).BindingContext as VIEW_ACH_INFO_ANEX;
+            viewModel.Items.Remove(elem);
+        }
+
+        private void Btn_Delete_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XpertMobileApp/XpertMobileApp/Views/Selector/Anex tiers/AnnexTiersSelectorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using XpertMobileApp.Api.ViewModels;
+using XpertMobileApp.Models;
+
+namespace XpertMobileApp.ViewModels
+{
+    public class AnnexTiersSelectorViewModel : CrudBaseViewModel<ACH_INFO_ANEX, VIEW_ACH_INFO_ANEX>
+    {
+
+        private List<VIEW_ACH_INFO_ANEX> currentEmballages;
+        public List<VIEW_ACH_INFO_ANEX> CurrentEmballages
+        {
+            get
+            {
+                return currentEmballages;
+            }
+            set
+            {
+                clearInfos();
+                currentEmballages = value;
+
+                if (currentEmballages == null) return;
+                foreach (var emb in currentEmballages)
+                {
+                    var itm = Items.Where(x => x.ID_LIGNE == emb.ID_LIGNE).FirstOrDefault();
+                    if (itm != null)
+                    {
+                        itm.NOM_TIERS = emb.NOM_TIERS;
+                        itm.QUANTITE_APPORT = emb.QUANTITE_APPORT;
+                        itm.CODE_DOC = emb.CODE_DOC;
+                    }
+                }
+            }
+        }
+
+        private void clearInfos()
+        {
+            CurrentEmballages.Clear();
+        }
+
+        public string SearchedText { get; set; } = "";
+
+        public AnnexTiersSelectorViewModel(string title= "" )
+        {
+            Title = title;
+        }
+
+        protected override void OnAfterLoadItems(IEnumerable<VIEW_ACH_INFO_ANEX> list)
+        {
+            base.OnAfterLoadItems(list);
+        }
+
+        protected override Dictionary<string, string> GetFilterParams()
+        {
+            Dictionary<string, string> result = base.GetFilterParams();
+
+            result.Add("searchText", SearchedText);
+            return result;
+        }
+    }
+}
