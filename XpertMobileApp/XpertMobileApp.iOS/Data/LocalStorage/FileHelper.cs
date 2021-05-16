14c6b3ad89887fa0fc29a390ff693b9b60e6acc9
d.sidali <d.sidali@xpertsoft-dz.com>
2021-05-16 17:19:06 +0000

    IOS implémentation

---
+++ XpertMobileApp/XpertMobileApp.iOS/Data/LocalStorage/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using ItMobileSolution.iOS.Data;
+using XpertMobileApp.Data;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ItMobileSolution.iOS.Data
+{
+	public class FileHelper : IFileHelper
+	{
+		public string GetLocalFilePath(string filename)
+		{
+			string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+			if (!Directory.Exists(libFolder))
+			{
+				Directory.CreateDirectory(libFolder);
+			}
+
+			return Path.Combine(libFolder, filename);
+		}
+	}
+}
---
+++ XpertMobileApp/XpertMobileApp.iOS/Services/DeviceInfos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using UIKit;
+using Xamarin.Forms;
+using XpertMobileApp.Data;
+using XpertMobileApp.Services;
+
+[assembly: Dependency(typeof(XpertMobileApp.iOS.Services.DeviceInfos))]
+namespace XpertMobileApp.iOS.Services
+{
+    public class DeviceInfos : IDeviceInfos
+    {
+        public string GetSecureOsId()
+        {
+            return UIDevice.CurrentDevice.IdentifierForVendor.AsString();
+        }
+
+        public bool HasPermission()
+        {
+            return true;
+        }
+
+        public void RequestPermissions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
