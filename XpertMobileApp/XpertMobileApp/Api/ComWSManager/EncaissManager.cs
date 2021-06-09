ad7d1034112755bd6cb7792d9f514b7affb71562
m.ramzy <r.merzeg@xpertsoft-dz.com>
2021-06-09 10:21:39 +0000

    [Projet] XpertMobile Tache : Ajout encaiss manager pour synchroniser les encaissmenets et les decaissements

---
+++ XpertMobileApp/XpertMobileApp/Api/ComWSManager/EncaissManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xpert.Common.WSClient.Helpers;
+using Xpert.Common.WSClient.Model;
+using Xpert.Common.WSClient.Services;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Models;
+
+namespace XpertMobileApp.Api
+{
+    internal class EncaissManager : CrudService<View_TRS_ENCAISS>
+    {
+        public EncaissManager() : 
+            base(App.RestServiceUrl, "TRS_ENCAISS", App.User.Token)
+        {
+
+        }
+
+        public async Task<string> SyncEncaiss(List<View_TRS_ENCAISS> encaiss)
+        {
+            string url = GetActionUrl("SyncEncaiss");
+            return await WSApi2.PostAauthorizedValue<string, List<View_TRS_ENCAISS>>(url, encaiss, this.Token.access_token);
+        }
+    }
+}
