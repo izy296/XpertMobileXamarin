e2862bc18101c93be6f8ed9066623cc86644f254
m.ramzy <r.merzeg@xpertsoft-dz.com>
2021-06-14 15:16:45 +0000

    [Projet] XpertMobile Tache : Composant Retour produit

---
+++ XpertMobileApp/XpertMobileApp/Views/04-Comercial/Selectors/Lot/RetourProducts.xaml.cs
@@ -0,0 +1,195 @@
+﻿using Acr.UserDialogs;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Helpers;
+using XpertMobileApp.Models;
+
+namespace XpertMobileApp.Views._04_Comercial.Selectors.Lot
+{
+    //    [XamlCompilation(XamlCompilationOptions.Compile)]
+    //    public partial class RetourProducts : ContentPage
+    //    {
+    //        public RetourProducts()
+    //        {
+    //            InitializeComponent();
+    //        }
+    //    }
+    //}
+    public partial class RetourProducts : PopupPage
+    {
+
+        RetourProductViewModel viewModel;
+        List<View_STK_STOCK> SelectedlistLot;
+        public string CurrentStream { get; set; }
+        public string CodeTiers
+        {
+            get
+            {
+                return viewModel.CodeTiers;
+            }
+            set
+            {
+                viewModel.CodeTiers = value;
+            }
+        }
+
+        public string AutoriserReception
+        {
+            get
+            {
+                return viewModel.AutoriserReception;
+            }
+            set
+            {
+                viewModel.AutoriserReception = value;
+            }
+        }
+
+        public RetourProducts(string stream)
+        {
+            InitializeComponent();
+            CurrentStream = stream;
+            BindingContext = viewModel = new RetourProductViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // if (viewModel.Items.Count == 0)
+            viewModel.LoadItemsCommand.Execute(null);
+
+            foreach (var item in viewModel.Items)
+            {
+                item.SelectedQUANTITE = 0;
+            }
+        }
+
+        private async void OnClose(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+        }
+
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // btnSelect.IsEnabled = true;
+            // btnRemove.IsEnabled = viewModel.SelectedItem != null && viewModel.SelectedItem.SelectedQUANTITE > 0;
+
+            // if (viewModel.SelectedItem != null)
+            //  {
+            //      viewModel.SelectedItem.SelectedQUANTITE += 1;
+            //  }
+        }
+
+        private async void btn_Search_Clicked(object sender, EventArgs e)
+        {
+            viewModel.LoadItemsCommand.Execute(null);
+        }
+
+        private async void btnSelect_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAsync();
+            if (viewModel.SelectedItem != null)
+            {
+                MessagingCenter.Send(this, CurrentStream, SelectedlistLot);
+                SelectedlistLot = null;
+            }
+        }
+
+        private void btnRemove_Clicked(object sender, EventArgs e)
+        {
+            if (viewModel.SelectedItem != null)
+            {
+                // viewModel.SelectedItem.SelectedQUANTITE = 0; // -=
+                // btnRemove.IsEnabled = viewModel.SelectedItem.SelectedQUANTITE > 0;
+
+                // MessagingCenter.Send(this, CurrentStream, viewModel.SelectedItem);
+                // App.MsgCenter.SendAction(this, CurrentStream, "REMOVE", MCDico.REMOVE_ITEM, viewModel.SelectedItem);
+            }
+        }
+
+        private async void ItemsListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (viewModel.SelectedItem != null)
+            {
+                if (SelectedlistLot == null)
+                {
+                    SelectedlistLot = new List<View_STK_STOCK>();
+                }
+                if (viewModel.SelectedItem.OLD_QUANTITE > 0)
+                {
+                    if (viewModel.SelectedItem.SelectedQUANTITE < viewModel.SelectedItem.OLD_QUANTITE)
+                    {
+                        if (SelectedlistLot.Contains(viewModel.SelectedItem))
+                        {
+                            try
+                            {
+                                SelectedlistLot.Where(x => x.ID_STOCK == viewModel.SelectedItem.ID_STOCK).FirstOrDefault().SelectedQUANTITE += 1;
+                            }
+                            catch
+                            {
+                            }
+                        }
+                        else
+                        {
+                            viewModel.SelectedItem.SelectedQUANTITE += 1;
+                            SelectedlistLot.Add(viewModel.SelectedItem);
+                        }
+                        //MessagingCenter.Send(this, CurrentStream, viewModel.SelectedItem);
+                        UpdateTotaux();
+                    }
+                    else
+                    {
+                        await UserDialogs.Instance.AlertAsync(" Quantité stock insuffisante ! \n La quantité stock = " + viewModel.SelectedItem.OLD_QUANTITE, AppResources.alrt_msg_Alert, AppResources.alrt_msg_Ok);
+                    }
+                }
+                else
+                {
+                    await UserDialogs.Instance.AlertAsync(" Quantité stock insuffisante ! \n La quantité stock = " + viewModel.SelectedItem.OLD_QUANTITE, AppResources.alrt_msg_Alert, AppResources.alrt_msg_Ok);
+                }
+            }
+        }
+
+        private void DelQte_Clicked(object sender, EventArgs e)
+        {
+            var lot = ((sender as Button).BindingContext as View_STK_STOCK);
+            SelectedlistLot.Remove(lot);
+            lot.SelectedQUANTITE = 0;
+            App.MsgCenter.SendAction(this, CurrentStream, "REMOVE", MCDico.REMOVE_ITEM, viewModel.SelectedItem);
+            UpdateTotaux();
+        }
+
+        private QteUpdater QteUpdater;
+        private async void DelUpdate_Clicked(object sender, EventArgs e)
+        {
+            var lot = ((sender as Button).BindingContext as View_STK_STOCK);
+            QteUpdater = new QteUpdater(lot);
+            QteUpdater.LotInfosUpdated += OnLotInfosUpdated;
+            await PopupNavigation.Instance.PushAsync(QteUpdater);
+        }
+
+        private void OnLotInfosUpdated(object sender, LotInfosEventArgs e)
+        {
+            var item = sender as View_STK_STOCK;
+            item.SelectedPrice = e.Price;
+            item.SelectedQUANTITE = e.Quantity;
+
+            UpdateTotaux();
+            MessagingCenter.Send(this, CurrentStream, item);
+        }
+
+        private void UpdateTotaux()
+        {
+            viewModel.TotalSelected = viewModel.Items.Where(e => e.SelectedQUANTITE > 0).Sum(e => e.SelectedQUANTITE * e.SelectedPrice);
+        }
+    }
+}
