fe1f646965ee7091b70913e510328020ff8a8758
k.hamza <h.khalfaoui@xpertsoft-dz.com>
2022-02-16 15:29:33 +0000

    changer AppResources

--- XpertMobileApp/XpertMobileApp/AppResources.Designer.cs
+++ XpertMobileApp/XpertMobileApp/AppResources.Designer.cs
@@ -39,7 +39,7 @@ namespace XpertMobileApp {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XpertMobileApp.AppResources", typeof(AppResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("XpertMobileAppManafiaa.AppResources", typeof(AppResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
