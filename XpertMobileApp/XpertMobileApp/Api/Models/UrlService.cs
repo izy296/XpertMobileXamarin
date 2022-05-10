3570852a6178e659d9a02090e98b2ef2492008f3
a.nadjib <n.annane@xpertsoft-dz.com>
2022-05-10 15:43:13 +0000

    [Url service ]

---
+++ XpertMobileApp/XpertMobileApp/Api/Models/UrlService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XpertMobileApp.Api.Models
+{
+    public class UrlService
+    {//Urlservice Model
+        public bool Selected
+        {
+            get;
+            set;
+        }
+        public string DisplayurlService
+        {
+            get;
+            set;
+        }
+
+    }
+}
