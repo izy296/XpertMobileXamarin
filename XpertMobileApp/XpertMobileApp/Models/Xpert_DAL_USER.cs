47c127a99117efa3f289c7afedfb94e5e7491477
z.ramzi <r.zeghida@xpertsoft-dz.com>
2022-05-11 10:47:48 +0000

    [MODIFICATION] https://app.clickup.com/t/2w8qfd0 ajouté view_SYS_USER

--- XpertMobileApp/XpertMobileApp/Models/Xpert_DAL_USER.cs
+++ XpertMobileApp/XpertMobileApp/Models/Xpert_DAL_USER.cs
@@ -25,4 +25,23 @@ namespace XpertMobileApp.Models
         public string TEL_USER { get; set; } // varchar(32)
         public string CODE_TIERS { get; set; }
     }
+
+    public partial class View_SYS_USER
+    {
+        public int ID { get; set; }
+        public string ID_USER { get; set; } // varchar(32)
+        public string PASS_USER { get; set; } // varchar(32)
+        public string CODE_GROUPE { get; set; } // varchar(32)
+        public bool ETAT_USER { get; set; } // varchar(5)                        
+        public string CB_USER { get; set; } // varchar(32)
+        public string CODE_COMPTE { get; set; } // varchar(32)
+        public string OWNER { get; set; } // varchar(32)
+        public DateTime? MODIFIED_ON { get; set; } // datetime(3)
+        public DateTime? CREATED_ON { get; set; } // datetime(3)
+        public string MODIFIED_BY { get; set; } // varchar(32)
+        public string CREATED_BY { get; set; } // varchar(32)
+        public int ORDRE_USER { get; set; } // int 
+        public string TEL_USER { get; set; } // varchar(32)
+        public string CODE_TIERS { get; set; }
+    }
 }
