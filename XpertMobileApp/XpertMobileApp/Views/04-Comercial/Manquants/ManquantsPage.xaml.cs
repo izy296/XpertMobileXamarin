6f4bdec243b9934ac84e3df295fe1b73ab63b398
b.fares <f.bourrich@xpertsoft-dz.com>
2022-06-02 08:09:22 +0000

    [modifier] https://app.clickup.com/t/2xpfbz8 Ajouter quelques commentaires Changer le nom de variable SelectedTiers en SelectedUsers

--- XpertMobileApp/XpertMobileApp/Views/04-Comercial/Manquants/ManquantsPage.xaml.cs
+++ XpertMobileApp/XpertMobileApp/Views/04-Comercial/Manquants/ManquantsPage.xaml.cs
@@ -21,7 +21,7 @@ namespace XpertMobileApp.Views
 	{
         ManquantsViewModel viewModel;      
         public string CurrentStream = Guid.NewGuid().ToString();       
-        public View_SYS_USER SelectedTiers { get; set; }
+        public View_SYS_USER SelectedUsers { get; set; }
         private UserSelector itemSelector;       
         public ManquantsPage()
 		{
@@ -30,8 +30,8 @@ namespace XpertMobileApp.Views
             BindingContext = viewModel = new ManquantsViewModel();
             MessagingCenter.Subscribe<UserSelector, View_SYS_USER>(this, CurrentStream, async (obj, selectedItem) =>
             {
-               viewModel.SelectedTiers = selectedItem;
-               ent_SelectedTiers.Text = selectedItem.ID_USER;
+               viewModel.SelectedUsers = selectedItem;
+                ent_SelectedUsers.Text = selectedItem.ID_USER;
             });           
         }
         async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
@@ -50,9 +50,12 @@ namespace XpertMobileApp.Views
             if (viewModel.Types.Count == 0)
                 viewModel.LoadExtrasDataCommand.Execute(null);
         }
+        /// <summary>
+        /// Fonction qui permet de vider les champs de filtre
+        /// </summary>
         private void ClearFilters()
         {
-            ent_SelectedTiers.Text = "";
+            ent_SelectedUsers.Text = "";
             TypesPicker.SelectedItem = TypesPicker.ItemsSource[0];
             TypesProduitPicker.SelectedItem = TypesProduitPicker.ItemsSource[0];
             StockMin.IsChecked = false;
@@ -61,27 +64,52 @@ namespace XpertMobileApp.Views
         {
             FilterPanel.IsVisible = !FilterPanel.IsVisible;
         }
+        /// <summary>
+        /// Fonction qui permet d'appliquer un filtre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btn_ApplyFilter_Clicked(object sender, EventArgs e)
         {        
             viewModel.LoadItemsCommand.Execute(null);
             FilterPanel.IsVisible = false;
         }
+        /// <summary>
+        /// Fonction qui permet d'afficher une popup pour l'ajout d'un manquant
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void AjoutItem_Clicked(object sender, EventArgs e)
         {
             NewManquantPopupPage form = new NewManquantPopupPage();
             await PopupNavigation.Instance.PushAsync(form);
         }
+        /// <summary>
+        /// Fonction qui permet de fermer la page de filtre 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private  void btn_CancelFilter_Clicked(object sender, EventArgs e)
         {           
             FilterPanel.IsVisible = false;
             ClearFilters();
             viewModel.LoadItemsCommand.Execute(null);          
-        }        
+        }
+        /// <summary>
+        /// Fonction qui permet d'afficher la listes des utilisateurs dans le filtre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void btn_Select_Clicked(object sender, EventArgs e)
         {
             itemSelector.SearchedType = "";
             await PopupNavigation.Instance.PushAsync(itemSelector);
         }
+        /// <summary>
+        /// Fonction qui permet de detecter le changement de checkbox (afficher le stock minimum)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
         {
             if (sender is CheckBox)
