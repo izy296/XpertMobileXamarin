a5a1f4de0e2f9fa74a0793541c54787851f89772
a.nadjib <n.annane@xpertsoft-dz.com>
2022-09-18 14:50:50 +0000

    [Modification] https://app.clickup.com/t/3hqd10c  * ajout du nouveau filtre dans la page rotation de produit

--- XpertMobileApp/XpertMobileApp/Views/04-Comercial/Produits/Rotation/RotationDesProduitsPage.xaml.cs
+++ XpertMobileApp/XpertMobileApp/Views/04-Comercial/Produits/Rotation/RotationDesProduitsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XpertMobileApp.Base;
@@ -11,27 +12,11 @@ namespace XpertMobileApp.Views
     public partial class RotationDesProduitsPage : XBasePage
     {
         RotationDesProduitsViewModel viewModel;
-
-        private bool opened = false;
-
-        double translateHide = -270;
-        double translateShow = 0;
         public RotationDesProduitsPage()
         {
             InitializeComponent();
 
             BindingContext = viewModel = new RotationDesProduitsViewModel();
-            //if (App.Settings.Language == "ar")
-            //{
-            //    translateHide = App.Current.MainPage.Width;
-            //    translateShow = App.Current.MainPage.Width - 360;
-            //}
-
-            filterLayout.TranslateTo(translateHide, 0);
-            FilterScroll.TranslateTo(translateHide, 0);
-
-
-            FilterScroll.Focused += FilterScroll_Focused;
         }
 
         private void FilterScroll_Focused(object sender, FocusEventArgs e)
@@ -79,68 +64,16 @@ namespace XpertMobileApp.Views
             viewModel.LoadItemsCommand.Execute(null);
         }
 
-        private void Filter_Clicked(object sender, EventArgs e)
-        {
-            hideFilter();
-        }
-
         private void btn_ApplyFilter_Clicked(object sender, EventArgs e)
         {
-            hideFilter();
             viewModel.LoadItemsCommand.Execute(null);
         }
 
-        private void btn_CancelFilter_Clicked(object sender, EventArgs e)
+        private async void ShowHideFilter(object sender, EventArgs e)
         {
-            hideFilter();
-            //quantiteG.IsChecked = false;
-            //quantiteE.IsChecked = false;
-            //quantiteL.IsChecked = false;
-            viewModel.SearchedText = "";
-            viewModel.ClearFilters();
-            viewModel.LoadItemsCommand.Execute(null);
-        }
-
-        private void ComptePicker_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-
-
-
-        private void showHideFilter(object sender, EventArgs e)
-        {
-            if (opened)
-            {
-                filterLayout.TranslateTo(translateHide, 0);
-                FilterScroll.TranslateTo(translateHide, 0);
-            }
-            else
-            {
-                filterLayout.TranslateTo(translateShow, 0);
-                FilterScroll.TranslateTo(translateShow, 0);
-            }
-            opened = !opened;
-        }
-
-
-        private void showFilter()
-        {
-
-            filterLayout.TranslateTo(translateShow, 0); 
-            FilterScroll.TranslateTo(translateShow, 0);
-            opened = !opened;
-           
-        }
-
-        private void hideFilter()
-        {
-
-            filterLayout.TranslateTo(translateHide, 0);
-            FilterScroll.TranslateTo(translateHide, 0);
-            opened = !opened;
-
+            RotationPopupFilter filter = new RotationPopupFilter(viewModel);
+            //Load data for the first time ...
+            await PopupNavigation.Instance.PushAsync(filter);
         }
 
         public override void HandleSearchBarTextChanged(object sender, string searchBarText)
@@ -148,7 +81,5 @@ namespace XpertMobileApp.Views
             viewModel.SearchedText = searchBarText;
             viewModel.LoadItemsCommand.Execute(null);
         }
-
-
     }
 }
