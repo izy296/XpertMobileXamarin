eef8fba9030a5e06854556ce512659b43b9c64ed
k.hamza <h.khalfaoui@xpertsoft-dz.com>
2023-01-02 13:26:28 +0000

    add DaytimeDelivery

---
+++ XpertMobileApp/XpertMobileApp/Views/06-Manafiaa/DaytimeDelivery/DaytimeDelivery_DashBord.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Syncfusion.Data.Extensions;
+using Syncfusion.DataSource.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Models;
+using XpertMobileApp.SQLite_Managment;
+using XpertMobileApp.ViewModels;
+using XpertMobileAppManafiaa.SQLite_Managment;
+
+namespace XpertMobileAppManafiaa.Views._06_Manafiaa.DaytimeDelivery
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DaytimeDelivery_DashBord : ContentPage
+    {
+        ItemDetailViewModel<View_LIV_TOURNEE> viewModel;
+
+        public DaytimeDelivery_DashBord()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            View_LIV_TOURNEE Tournne = await SyncManager.ClotureTournee();
+            if (Tournne != null)
+            {
+                BindingContext = this.viewModel = new ItemDetailViewModel<View_LIV_TOURNEE>(Tournne);
+
+            }
+        }
+    }
+}
