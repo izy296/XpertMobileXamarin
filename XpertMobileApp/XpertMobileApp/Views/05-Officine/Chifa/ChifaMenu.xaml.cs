c6fb65a59ce507a274fc2051ca050dee53c3b596
a.nadjib <n.annane@xpertsoft-dz.com>
2023-01-30 08:35:05 +0000

    [Ajout]https://app.clickup.com/t/8669dnmqu Création d'une page qui contient le menu chifa.

---
+++ XpertMobileApp/XpertMobileApp/Views/05-Officine/Chifa/ChifaMenu.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PancakeView;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp.Views._05_Officine.Chifa.FactureCHIFA;
+
+namespace XpertMobileApp.Views._05_Officine.Chifa
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public class MenuChifaItem
+    {
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public string BGCOLOR { get; set; }
+        public string ICON { get; set; }
+        public double NbFACTURE { get; set; }
+        public double MtTotal { get; set; }
+    }
+    public partial class ChifaMenu : ContentPage
+    {
+        private ObservableCollection<MenuChifaItem> menuItems;
+        public ObservableCollection<MenuChifaItem> MenuItems
+        {
+            get
+            {
+                return menuItems;
+            }
+            set
+            {
+                menuItems = value;
+                OnPropertyChanged("MenuItems");
+            }
+        }
+
+        public ChifaMenu()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            /* Initialisation du menu CHIFA*/
+            MenuItems = new ObservableCollection<MenuChifaItem>();
+
+            /* Ajout des items dans le menu CHIFA*/
+            MenuItems.Add(new MenuChifaItem
+            {
+                ID = 0,
+                TITLE = "Bordereau",
+                BGCOLOR = "#f0f0f0",
+            });
+
+            MenuItems.Add(new MenuChifaItem
+            {
+                ID = 1,
+                TITLE = "Aujourd'hui",
+                BGCOLOR = "#f0f0f0",
+            });
+
+        }
+
+        private void listView_SelectionChanged(object sender, Syncfusion.ListView.XForms.ItemSelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void NavigateFromCHIFAMenu(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = ((sender as PancakeView).BindingContext as MenuChifaItem).ID;
+                switch (id)
+                {
+                    case 1:
+                        Navigation.PushAsync(new RapportJournalierFactureCHIFA());
+                        break;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
