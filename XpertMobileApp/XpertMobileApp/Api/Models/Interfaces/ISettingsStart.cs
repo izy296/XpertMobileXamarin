21ea4796781ed1f3440f3b8a47ca56a5101693cf
a.nadjib <n.annane@xpertsoft-dz.com>
2023-02-22 16:06:47 +0000

    [Ajout]  * Dans le cas ou on veut reconnecté et on n'est pas connecté a un reseau , elle vous aides a choisir le bon reseau dans la page parametre reseau

---
+++ XpertMobileApp/XpertMobileApp/Api/Models/Interfaces/ISettingsStart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XpertMobileApp.Api.Models.Interfaces
+{
+    public interface ISettingsStart
+    {
+        void StartSettings();
+    }
+}
