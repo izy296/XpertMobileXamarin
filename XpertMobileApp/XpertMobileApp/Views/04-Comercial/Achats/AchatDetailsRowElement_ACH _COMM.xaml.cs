e5c37a4cf1e66e9bdf9538111040e89f30a2d11f
a.nadjib <n.annane@xpertsoft-dz.com>
2023-04-19 09:55:04 +0000

    [Correction] Ajout fichier manquant

---
+++ XpertMobileApp/XpertMobileApp/Views/04-Comercial/Achats/AchatDetailsRowElement_ACH _COMM.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Acr.UserDialogs;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp.DAL;
+using XpertMobileApp.ViewModels;
+using XpertMobileApp.Views._04_Comercial.Achats;
+using XpertMobileApp.Views._04_Comercial.TransfertDeFond;
+
+namespace XpertMobileApp.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AchatDetailsRowElement_ACH_COMM : ContentView
+    {
+        public AchatDetailsRowElement_ACH_COMM()
+        {
+            InitializeComponent();
+        }
+        //private async void showReclamation(object sender, EventArgs e)
+        //{
+        //    ReclamationPopupPage reclamationInfo = new ReclamationPopupPage(this.CodeReclamaion.Text.ToString());
+        //    UserDialogs.Instance.ShowLoading(AppResources.txt_Waiting);
+        //    await reclamationInfo.GetReclamation();
+        //    UserDialogs.Instance.HideLoading();
+        //    await PopupNavigation.Instance.PushAsync(reclamationInfo);
+        //}
+
+        private async void OnUpdateSwipeItemInvoked(object sender, EventArgs e)
+        {
+            var tr = (sender as SwipeItem)?.Parent?.Parent?.Parent?.BindingContext as View_ACH_DOCUMENT;
+            await Navigation.PushAsync(new NewAchatPage(item:tr));
+        }
+    }
+}
