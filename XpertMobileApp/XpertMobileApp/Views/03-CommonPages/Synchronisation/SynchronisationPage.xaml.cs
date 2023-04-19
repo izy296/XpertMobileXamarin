751ec153a2a247b9602ba852af34bbfaf8c5d454
a.nadjib <n.annane@xpertsoft-dz.com>
2023-04-19 09:33:50 +0000

    [Ajout][XM_DISTRIBUTION] https://app.clickup.com/t/8669vm8kw  * Ajout L'achat en mode offline  * AJout L'achat en mode online  * Création d'un template pour le detail d'un achat  * Ajouter l'etat du synchronisation de l'achat  * Création d'une nouvelle page pour ajouter un achat.  * Création d'une popup pour editer un achat  * ajouter lo colisage dans l'operation du création d'un achat  * Creation d'un data template selector pour le template d'achat_detail.

--- XpertMobileApp/XpertMobileApp/Views/03-CommonPages/Synchronisation/SynchronisationPage.xaml.cs
+++ XpertMobileApp/XpertMobileApp/Views/03-CommonPages/Synchronisation/SynchronisationPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XpertMobileApp.Api.Models.Interfaces;
 using XpertMobileApp.Models;
 using XpertMobileApp.SQLite_Managment;
 
@@ -25,13 +26,13 @@ namespace XpertMobileApp.Views._03_CommonPages.Synchronisation
         {
             try
             {
-                if (App.Online)
+                if (await App.IsConected())
                 {
                     Btn_Upload.IsEnabled = false;
                     Btn_Download.IsEnabled = false;
                     syncImage.RotateTo(350 * -360, 10 * 60 * 1000);
 
-                    SyncResumePoup popup = new SyncResumePoup();
+                    SyncResumeUploadPopup popup = new SyncResumeUploadPopup();
                     await PopupNavigation.Instance.PushAsync(popup);
                     popup.CheckSynchronisation();
 
@@ -42,8 +43,15 @@ namespace XpertMobileApp.Views._03_CommonPages.Synchronisation
                 }
                 else
                 {
-                    CustomPopup AlertPopup = new CustomPopup("Veuillez verifier votre connexion au serveur ! ", trueMessage: AppResources.alrt_msg_Ok);
+                    CustomPopup AlertPopup = new CustomPopup("Connexion introuvable, voulez-vous vous connecter  ? ", falseMessage: AppResources.exit_Button_No, trueMessage: "Oui");
                     await PopupNavigation.Instance.PushAsync(AlertPopup);
+                    if (await AlertPopup.PopupClosedTask)
+                    {
+                        if (AlertPopup.Result)
+                        {
+                            DependencyService.Get<ISettingsStart>().StartSettings();
+                        }
+                    }
                 }
 
             }
@@ -59,12 +67,15 @@ namespace XpertMobileApp.Views._03_CommonPages.Synchronisation
         {
             try
             {
-                if (App.Online)
+                if (await App.IsConected())
                 {
                     Btn_Upload.IsEnabled = false;
                     Btn_Download.IsEnabled = false;
-
                     syncImage.RotateTo(350 * 360, 10 * 60 * 1000);
+
+                    SyncResumeDownloadPopup popup = new SyncResumeDownloadPopup();
+                    await PopupNavigation.Instance.PushAsync(popup);
+
                     await SQLite_Manager.SynchroniseDownload();
                     syncImage.CancelAnimations();
                     Btn_Upload.IsEnabled = true;
@@ -72,10 +83,16 @@ namespace XpertMobileApp.Views._03_CommonPages.Synchronisation
                 }
                 else
                 {
-                    CustomPopup AlertPopup = new CustomPopup("Veuillez verifier votre connexion au serveur ! ", trueMessage: AppResources.alrt_msg_Ok);
+                    CustomPopup AlertPopup = new CustomPopup("Connexion introuvable, voulez-vous vous connecter  ? ", falseMessage: AppResources.exit_Button_No, trueMessage: "Oui");
                     await PopupNavigation.Instance.PushAsync(AlertPopup);
+                    if (await AlertPopup.PopupClosedTask)
+                    {
+                        if (AlertPopup.Result)
+                        {
+                            DependencyService.Get<ISettingsStart>().StartSettings();
+                        }
+                    }
                 }
-
             }
             catch (Exception ex)
             {
