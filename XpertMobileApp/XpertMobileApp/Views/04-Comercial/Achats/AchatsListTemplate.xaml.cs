0b68ca33e170b7ee856eb3b07ac2440d3ad095c0
a.nadjib <n.annane@xpertsoft-dz.com>
2023-04-30 15:59:25 +0000

    [Correction][XM_DISTRIBUTION] https://app.clickup.com/t/866a2akwy remplacer le message connexion etablie avec succés par un toast

---
+++ XpertMobileApp/XpertMobileApp/Views/04-Comercial/Achats/AchatsListTemplate.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XpertMobileApp.DAL;
+using XpertMobileApp.Views._04_Comercial.Achats;
+
+namespace XpertMobileApp.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AchatsListTemplate : ContentView
+    {
+        public AchatsListTemplate()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowDetailSales(object sender, EventArgs e)
+        {
+            try
+            {
+                View_ACH_DOCUMENT item = (sender as Frame).Parent.Parent.Parent.BindingContext as View_ACH_DOCUMENT;
+                Task.Run(async () => await Navigation.PushAsync(new NewAchatPage(item:item)));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void OnUpdateSwipeItemInvoked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
