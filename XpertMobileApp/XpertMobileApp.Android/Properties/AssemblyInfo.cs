0eafc00bae1a4c0a438ee042c48a948b2314f22a
a.jaffar <d.amir@xpertsoft-dz.com>
2024-03-08 07:18:43 +0000

    [MODIFICATION] Commit des differents AndroidManifest.xml

--- XpertMobileApp/XpertMobileApp.Android/Properties/AssemblyInfo.cs
+++ XpertMobileApp/XpertMobileApp.Android/Properties/AssemblyInfo.cs
@@ -6,15 +6,16 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XpertMobileApp.Android")]
+[assembly: AssemblyTitle("com.xpertsoft.ComMobileAppOfficine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XpertMobileApp.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("com.xpertsoft.ComMobileAppOfficine")]
+[assembly: AssemblyCopyright("Copyright XpertSoft©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -22,8 +23,8 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.791")]
+[assembly: AssemblyFileVersion("2.791")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
